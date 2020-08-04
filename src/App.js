462ceaa3257776966017c474b8c3399d1d0c7ba9
beckhamabo <634264926@qq.com>
2020-08-04 13:56:03 +0800

    usereffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router,Route, Link } from 'react-router-dom'
+function Index() {
+  useEffect(()=>{
+    console.log('userEffect=>This is Index')
+    return()=>{
+      console.log('Index 走了')
+    }
+  },[])//解除绑定
+  return (
+    <h2>no!1</h2>
+  )
+}
+function List() {
+  useEffect(()=>{
+    console.log('userEffect=>This is List')
+  })
+  return (<h2>ListPage</h2>)
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+          <li>
+            <Link Link to='/'>首页
+          </Link>
+          </li>
+          <li>
+            <Link Link to='/list/'>列表
+          </Link>
+          </li>
+        </ul>
+        <Route path='/' exact component={Index}></Route>
+        <Route path='/list/' exact component={List}></Route>
+
+      </Router>
+
     </div>
   );
 }
